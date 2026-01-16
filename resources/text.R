b3cc79756c6aa23d310d6d173d2387a085f91721
Sjur Øfsteng <sjurj@hotmail.com>
2026-01-16 16:31:21 +0100

    update PES

---
+++ resources/text.R
@@ -0,0 +1,58 @@
+
+
+
+Kompetanse for å vurdere totalbelastning, tilpasninger
+Troppsjef
+Lagfører
+Hva er arbeidskravet under vinterøvelsen/vardekriger?
+  - Hva er din kapasitet? Lav som mulig over tid?
+  - Hvilken intensitet bør man ligge på? Hvilket tempo skal man forflytte seg på? Disponere vekt i laget?
+  
+  - Lede belastning og ikke bare retning,.
+- leder mentalt overskudd til å lede andre og ta beslutninger, krever høyere fysisk krav for å stå i dette over tid.
+
+- Terreng, stigning, underlag kan man vite noe om og beregne sitt energiforbruk, bruk kartet?
+  
+  - 12-14% økt energiforbruk per 1% stigning
+- Hastighet, 6 km/timen på vanlig gange.
+- Bør ligge under 50% av sin Vo2max, over dette vil du få økt energiforbruk over tid på den samme intensiteten.
+Korrelerer med 15 min på 3000 m kravet.
+
+- Oppakning: 30-40% av sin vo2max ved lengre marsjer, dette ligger da på 300-400 kcal per time.
+Borg 11 marsj, relaer deg til den.
+
+
+-----------
+  
+  NATO report
+
+- 20-30% av egen kroppsvekt - sweet spot
+- mobilitet
+- kontroll på energiforbruk
+- redusere skaderisiko
+
+3 linje ligger muligens omkring dette..
+
+----------
+  
+  Praktiske råg for lagføreren:
+  - Bruk test resultatene aktivt i hvordan laget ditt ser ut. Hvordan bør den indre intensiteten være for hver person? Er dere i ulik form i gruppen?
+  - Tilpasninger på hele laget
+- Anvendelse av treningsprinsippene, stor spredning på gruppen. Ulik oppakning mellom personene når hastigheten skal være lik på gruppen, individuell tilpasning prinsippet.
+
+Lett fottøy:
+  - Reduserer energiforbruket
+- Ekstremiteter - tungt våte skotøy koster mere...
+- God ankelstøtte - hva er optimalt
+
+
+
+Reduserer FA
+- Økt skaderisiko
+- Beveger seg lenger fra reel situasjon - utøve jobb kravet
+- Lengre tid til progresjon i treningen
+- Krever også mer ressurser som tid og personell - individuell tilpasning og oppfølgning.
+
+
+
+- 1 kg økning = 4% økt energiforbruk
